cbc975fcf4b3c49961d0ee00d51c5a60c5099518
Dhruv Deshmukh <122702130+Justsomebody1234@users.noreply.github.com>
2024-02-09 14:58:01 +0400

    Update UART_rx.sv

--- Design/UART_rx.sv
+++ Design/UART_rx.sv
@@ -1,27 +1,55 @@
-module UART_rx #(parametre clk_freq = 1000000, baud_rate = 9600) (
+module UART_rx #(parameter clk_freq = 1000000, baud_rate = 9600) (
     input clk, rst, rx,
     output reg done,
     output reg [7:0] rxdata 
 );
 
-localparam clkcount = (clk_freq / baud_rate);
-
+localparam clkcount = clk_freq / baud_rate;
 integer count  = 0;
 integer counts = 0;
-
 reg slowclk = 0;
 
-enum bit[1:0] {idle = 2'b00, start = 2'b01} state; 
+// Define states
+localparam idle = 2'b00, start = 2'b01;
+reg [1:0] state = idle; 
 
 always @(posedge clk) begin
-
-    if (count < clkcount/2)
-        count <= count+1;
-    else begin 
+    if (rst) begin
+        count <= 0;
+        slowclk <= 0;
+    end else if (count < clkcount/2 - 1) begin
+        count <= count + 1;
+    end else begin 
         count <= 0;
         slowclk <= ~slowclk;
     end
+end
 
+always @(posedge slowclk or posedge rst) begin
+    if (rst) begin
+        rxdata <= 8'b0;
+        counts <= 0;
+        done <= 1'b0;
+        state <= idle;
+    end else case(state)
+        idle: begin 
+            rxdata <= 8'b0;
+            counts <= 0;
+            done <= 1'b0;
+            if(rx == 1'b0) state <= start;
+        end
+        start: begin
+            if(counts < 8) begin
+                rxdata <= {rx, rxdata[7:1]};
+                counts <= counts + 1;
+            end else begin
+                done <= 1'b1;
+                state <= idle;
+                counts <= 0;
+            end
+        end
+        default: state <= idle;
+    endcase
 end
     
 endmodule
